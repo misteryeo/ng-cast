d8b7719e33436a4c245cccb3a5185fefe09f85e2
misteryeo <andy.yeo@gmail.com>
2017-02-24 20:38:23 -0800

    Pass most of videoPlayer tests.

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -3,6 +3,15 @@ angular.module('video-player')
 .directive('videoPlayer', function() {
   return {
     // TODO
+    scope: {
+      video: '<',
+      onClick: '<'
+    },
+    controllerAs: 'ctrl',
+    bindToController: true,
+    controller: function($scope) {
+      console.log($scope);
+    },
     templateUrl: 'src/templates/videoPlayer.html'
   };
 });
