dfb990a6eac8be7130c058dc2f374a09777ce3e7
misteryeo <andy.yeo@gmail.com>
2017-02-25 11:02:41 -0800

    Implement click event for video entries to re-set current video

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,10 @@ angular.module('video-player')
   return {
     // TODO
     scope: {},
+    bindToController: true,
+    controllerAs: 'ctrl',
     templateUrl: 'src/templates/app.html',
+    // QUESTION: why do we use 'this' here instead of $scope
     controller: function($scope) {
       console.log($scope);
       this.videos = window.exampleVideoData;
@@ -13,9 +16,9 @@ angular.module('video-player')
       this.selectVideo = function() {};
       this.searchResults = function() {};
       this.currentVideo = this.videos[0];
-      this.onClick = function () { console.log('clicked'); };
+      this.onClick = function (video) { 
+        this.currentVideo = video;
+      }.bind(this);
     },
-    bindToController: true,
-    controllerAs: 'ctrl'
   };
 });
